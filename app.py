cac90c318e58235dda3d234053c5f4ad0fab0c12
kesh-khanna <65061082+kesh-khanna@users.noreply.github.com>
2023-05-31 11:37:23 +0200

    Started the front end. Basic username entry page and leaderboard can be seen in templates and app.py

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        username = request.form['username']
+        return redirect(url_for('leaderboard', username=username))
+    return render_template('index.html')
+
+
+@app.route('/leaderboard/<username>')
+def leaderboard(username):
+    # make calls to the Spotify API HERE
+    # Replace the code below with actual implementation
+    playlists = [
+        {'name': 'Playlist 1', 'rank': 1},
+        {'name': 'Playlist 2', 'rank': 2},
+        {'name': 'Playlist 3', 'rank': 3}
+    ]
+    return render_template('leaderboard.html', username=username, playlists=playlists)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ authentification.py
@@ -0,0 +1,6 @@
+import spotipy
+
+
+# request a users login and password from their spotify account
+def get_user_login():
+    pass
