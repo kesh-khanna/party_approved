9b86908f835a16155ff5c10c7fae8fa989352e36
kesh-khanna <65061082+kesh-khanna@users.noreply.github.com>
2023-05-31 15:39:30 +0200

    basic functionality added so that popularity can be calculated and displayed. More styling of the leaderboard, and checks for certain issues. TODO: fix the headers in leaderboard.html

--- app.py
+++ app.py
@@ -1,33 +1,92 @@
+import os
+
 from flask import Flask, render_template, request, redirect, url_for
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+from dotenv import load_dotenv
 
 app = Flask(__name__)
 
+load_dotenv()
 
-@app.route('/', methods=['GET', 'POST'])
+client_id = os.getenv("CLIENT_ID")
+client_secret = os.getenv("CLIENT_SECRET")
+
+# Set up Spotipy client credentials
+client_credentials_manager = SpotifyClientCredentials(client_id=client_id,
+                                                      client_secret=client_secret)
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+
+@app.route('/')
 def index():
-    if request.method == 'POST':
-        username = request.form['username']
-        return redirect(url_for('leaderboard', username=username))
     return render_template('index.html')
 
 
-@app.route('/leaderboard/<username>')
-def leaderboard(username):
-    # make calls to the Spotify API HERE
-    # Replace the code below with actual implementation
-    playlists = [
-        {'name': 'Playlist 1', 'rank': 1},
-        {'name': 'Playlist 2', 'rank': 2},
-        {'name': 'Playlist 2', 'rank': 2},
-        {'name': 'Playlist 2', 'rank': 2},
-        {'name': 'Playlist 2', 'rank': 2},
-        {'name': 'Playlist 2', 'rank': 2},
-        {'name': 'Playlist 2', 'rank': 2},
-        {'name': 'Playlist 2', 'rank': 2},
-        {'name': 'Playlist 3', 'rank': 3}
-    ]
-    return render_template('leaderboard.html', username=username, playlists=playlists)
+@app.route('/leaderboard', methods=['POST'])
+def leaderboard():
+    username = request.form['username']
+    playlists = get_user_playlists(username)
+    sorted_playlists = sort_playlists_by_pop(playlists)
+    return render_template('leaderboard.html', username=username, playlists=sorted_playlists)
+
+
+def get_user_playlists(username):
+    playlists = []
+    results = sp.user_playlists(username)
+
+    while results:
+        for playlist in results['items']:
+            playlists.append(playlist)
+            if len(playlists) >= 10:
+                break
+        if results['next']:
+            results = sp.next(results)
+        else:
+            results = None
+
+    return playlists
+
+
+def sort_playlists_by_pop(playlists):
+    sorted_playlists = []
+
+    for playlist in playlists:
+        playlist_id = playlist['id']
+        playlist['pop'] = calculate_playlist_pop(playlist_id)
+        playlist['cover_image'] = get_playlist_cover_image(playlist_id)
+        sorted_playlists.append(playlist)
+
+    sorted_playlists.sort(key=lambda x: x['pop'], reverse=True)
+    return sorted_playlists
+
+
+def calculate_playlist_pop(playlist_id):
+    tracks = sp.playlist_items(playlist_id)['items']
+    pop_sum = 0
+    num_tracks = 0
+
+    for track in tracks:
+        if 'track' in track:
+            pop = track['track']['popularity']
+            pop_sum += pop
+            num_tracks += 1
+
+    if num_tracks > 0:
+        average_popularity = round(pop_sum / num_tracks, 2)
+        return average_popularity
+    else:
+        return 0
+
+
+def get_playlist_cover_image(playlist_id):
+    playlist = sp.playlist(playlist_id, fields='images')
+    if 'images' in playlist and len(playlist['images']) > 0:
+        return playlist['images'][0]['url']
+    else:
+        return None
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
