6fe46e070887648fe7f79d8225679eec11bc5265
Pantju Albert Cooper Ralapiny Nam <pantju.nam@adelaide.edu.au>
2023-06-08 14:23:50 +0200

    added functions

--- connect.py
+++ connect.py
@@ -19,7 +19,6 @@ def config(filename='database.ini', section='postgresql'):
 
     return db
 
-
 def connect():
     """ Connect to the PostgreSQL database server """
     conn = None
@@ -74,7 +73,7 @@ def connect():
             print('Database connection closed.')
 
 
-def insert_playlists(playlists):
+def insert_playlists(playlists, username):
     sql = """ INSERT INTO Playlists (PlaylistID, Username, PlaylistName, Image, Score)
             VALUES(%s, %s, %s, %s, %s);"""
     conn = None
@@ -87,11 +86,10 @@ def insert_playlists(playlists):
 
             playlist_ID = playlist['id']
             playlist_Name = playlist['name']
-            image = 000000000000
+            image = playlist['cover_image']
             score = playlist['pop']
 
-            cur.execute(sql, (playlist_ID, Username_char, playlist_Name, image, score))
-        # cur.execute(sql, ('22zlrw75elsb2d2i3ftjdybly', 'asdasd', 'asdasd', 0, 2))
+            cur.execute(sql, (playlist_ID, username, playlist_Name, image, score))
 
         conn.commit()
         cur.close()
@@ -119,6 +117,53 @@ def insert_user(username):
         # commit the changes to the database
         conn.commit()
         # close communication with the database
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+def get_top_playlists(top_num = 25):
+    conn = None
+    try:
+        params = config()
+        conn = psycopg2.connect(**params)
+        cur = conn.cursor()
+        sql = """SELECT Playlistname, Username, Score 
+        FROM Playlists
+        ORDER BY SCORE
+        DESC LIMIT %s;"""
+        cur.execute(sql, (top_num,))
+
+        rows = cur.fetchall()
+
+        cur.close()
+        conn.commit()
+
+        return rows #returns a list of tuples
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+
+def refresh_playlist(playlist):
+    #Playlists passed must already be sorted
+    conn = None
+    try:
+        params = config()
+        conn = psycopg2.connect(**params)
+        cur = conn.cursor()
+        sql = """ UPDATE Playlists
+        SET Score = %s
+        WHERE PlaylistID = %s;"""
+
+        cur.execute(sql, (playlist['pop'], playlist['id'],))
+
+        conn.commit()
         cur.close()
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
